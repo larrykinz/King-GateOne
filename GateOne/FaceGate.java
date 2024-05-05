33bf44dae79a3f410c51b09c31bae5e227b95046
larrykinz <kingadesanya666@gmail.com>
2024-05-05 21:39:13 +0100

    adding new file

--- GateOne/FaceGate.java
+++ GateOne/FaceGate.java
@@ -1,5 +1,5 @@
-public class FaceGate{
-	public static void main(String... args){
-	System.out.print("Hello Word");
+public class faceGate{
+ public static void main(String... args){
+System.out.print("Hello Word");
 }
-} 
+}
